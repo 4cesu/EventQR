98c7dc5d54d976eb7039e27fcdc30788e6ae39ea
ivebaan <rodayban@gmail.com>
2026-05-23 17:07:34 +0800

    feat(android): add follow-up source codes for drawable resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.thedavelopers.eventqr"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.thedavelopers.eventqr"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
